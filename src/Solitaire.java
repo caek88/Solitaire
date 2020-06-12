ff0a29ce2541ad0a7ec5e38d0519e9f87b0754e4
caek88 <35225849+caek88@users.noreply.github.com>
2020-06-11 22:50:10 -0400

    Added Movecard method
    Added moveCard Method that returns whether or not a card is allowed to be moved to the specified location.

--- src/Solitaire.java
+++ src/Solitaire.java
@@ -1,5 +1,46 @@
-import java.awt.BorderLayout;
+import java.util.ArrayList;
+
 public class Solitaire extends CardDisplay {
+	private int columnXPos[] = {20, 120, 220, 320, 420, 520, 620};
+	private int topPos[] = {320, 420, 520, 620};
+	private Column cardColumns[] = new Column[7];
+	private Top cardsTop[] = new Top[4];
+	private Deck cardDeck = new Deck();
+	
+	//Locations: 7 rows: 0-6, 4 top positions: 7-10
+	public boolean moveCard(Card c, int location) {//Returns true if it allowed to move the card to the specified location
+		if (location < 7 && location > 0) {
+			Card topCard = cardColumns[location].visibleCards.get(cardColumns[location].visibleCards.size() - 1);
+			if (c.getRank() == topCard.getRank() - 1) {
+				if (c.getSuit() == Card.CLUBS || c.getSuit() == Card.SPADES) {
+					if (topCard.getSuit() == Card.HEARTS || topCard.getSuit() == Card.DIAMONDS) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+				if (c.getSuit() == Card.HEARTS || c.getSuit() == Card.DIAMONDS) {
+					if (topCard.getSuit() == Card.CLUBS || topCard.getSuit() == Card.SPADES) {
+						return true;
+					} else {
+						return false;
+					}
+				}
+			}
+		}
+		if (location >= 7 && location < 11) {
+			Card topCard = cardColumns[location].visibleCards.get(cardColumns[location].visibleCards.size() - 1);
+			return (c.getRank() == topCard.getRank() - 1 && c.getSuit() == topCard.getSuit());
+		}
+		return false;
+	}
+	private class Column {
+		public ArrayList<Card> bottomCards = new ArrayList<Card>();
+		public ArrayList<Card> visibleCards = new ArrayList<Card>();
+	}
+	private class Top {
+		public ArrayList<Card> cardStack = new ArrayList<Card>();
+	}
 	public void cardClicked(Card c) {
 		System.out.println(c.getRank());
 		System.out.println(c.getSuit());
@@ -8,7 +49,9 @@ public class Solitaire extends CardDisplay {
 	}
 	public Solitaire() {
 		super(800, 500, "Solitaire");
+		cardDeck.shuffle();
 	}
+	
 	public static void main(String args[]) {
 		Solitaire test = new Solitaire();
 		Deck testDeck = new Deck();
